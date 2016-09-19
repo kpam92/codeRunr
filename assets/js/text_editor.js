47ff3525b10471486758895f83476b75bf6f03fd
Jack Tilly <jackftilly@gmail.com>
2016-09-19 14:02:25 -0700

    styled a lot of the main page '

--- assets/js/text_editor.js
+++ assets/js/text_editor.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   let myCodeMirror = CodeMirror(document.getElementById('editor'), {
     mode: "javascript",
-    lineNumbers: true
+    lineNumbers: true,
+    autoCloseBrackets: true
   });
+  let windowHeight = window.innerHeight - 20;
+  document.getElementById('editor').style.height = `${windowHeight}px`
+  document.getElementById('output').style.height = `${windowHeight}px`
   let runButton = document.getElementById('run');
   runButton.addEventListener('click', () => run(myCodeMirror))
 });
@@ -12,5 +16,5 @@ function run(codemirror) {
   let code = codemirror.getValue();
   let value = eval(code);
   let output = document.getElementById('output');
-  output.innerHTML = value;
+  output.children[0].innerHTML = value;
 }
