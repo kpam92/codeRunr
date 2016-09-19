e525096260e02ed1fedd5ea70298116dcfe39715
Jack Tilly <jackftilly@gmail.com>
2016-09-19 11:43:39 -0700

    began configuring code to run

--- assets/js/text_editor.js
+++ assets/js/text_editor.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  var myCodeMirror = CodeMirror(document.body);
+  let myCodeMirror = CodeMirror(document.getElementById('editor'), {
+    mode: "javascript",
+    lineNumbers: true
+  });
+  let runButton = document.getElementById('run');
+  runButton.onClick(run);
 });
+
+
+function run() {
+  let textarea = document.getElementById('editor');
+  let code = textarea.text();
+  let functionToRun = putInFunction(code);
+  functionToRun();
+}
+
+function putInFunction(code) {
+  debugger;
+  let stuff = () => {
+    code
+  }
+  return stuff;
+}
