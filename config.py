b84ed8ec6576bb60baca16e65ea2bfea5809bc05
Jack Tilly <jackftilly@gmail.com>
2016-09-26 17:57:05 -0700

    added to database config

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','postgres://ctrqofwsvyrxvn:q7MlX9AfLHSCqCrODmXX9OpraO@ec2-54-243-201-3.compute-1.amazonaws.com:5432/d2v507p6853v9t')
 
 
 class StagingConfig(Config):
